2367e31c9663a6ca186df777078dc8c80836f09e
Michal Svanda <michal@astronomie.cz>
2022-06-30 21:57:05 +0200

    Completed translation of the comments to English (or some language inspired by English :-) ).

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ redukce=np.array([-0.09048021, -0.07796618, -0.03477956])   # fit with errors in
 redukce1=1  # reduce during the evaluation period, default=1
 redukce2=0 # reduce during the prediction period, default=0
 
-Npruchodu=2 # how many passes
+Npruchodu=2 # how many passes; default=2
 
 ################################################################################################
 
@@ -79,30 +79,30 @@ def nanstd(ar):
         return np.nanstd(ar)
 
 # prepare the list of available observers:
-# HERE
-# pozorovatele, kteri jsou k dispozici, v "testovani" jsou jejich unikatni jmena
+# pre-sort observers and chose only their unique names. Those are stored in the list "testovani"
 pozorovatele=db['Pozorovatel']
 testovani=list(pozorovatele.drop_duplicates())
 print(len(testovani))
 
-# vyber pozorovatelu, kteri maji alespon 100 pozorovani, ti jsou pak v dlouhodobi
+# preselection of the observers that have at least 100 observations each. Those are in the list "dlouhodobi"
+# these are not used further by default, however they might be useful 
+
 dlouhodobi=copy.deepcopy(testovani)
 for pozorovatel in testovani:
-    # print(pozorovatel)
     print(pozorovatel, ' ', len(db[db['Pozorovatel']==pozorovatel]))
     if len(db[db['Pozorovatel']==pozorovatel]) < 100:
         dlouhodobi.remove(pozorovatel)
         
-# urezat jine nez dlouhodobe pozorovatele
+# cut all observers except for those long-term with more than 100 records
 db=db[db['Pozorovatel'].apply((lambda x: x in dlouhodobi) )]
 
 
-# pozorovatele, kteri jsou k dispozici
+# store the available observers (unique names) into yet another list
 
 referencni=copy.deepcopy(testovani)
 #referencni=copy.deepcopy(dlouhodobi)
 
-# referencni.remove(kralik)
+# initiate the table of personal coefficients
 
 table_k=pd.DataFrame({'Datum':[], 'Jmeno':[], 'k_f':[], 'k_g':[], 'Pruchod':[]})
 
@@ -113,91 +113,102 @@ table_k=pd.DataFrame({'Datum':[], 'Jmeno':[], 'k_f':[], 'k_g':[], 'Pruchod':[]})
 ###
 # 1. a reference observer
 
+# start from a named reference observer. Here, e.g., 'ZLOCH FRANTIŠEK'
 vzorny_pozorovatel='ZLOCH FRANTIŠEK'
 from sunspots import initial_reference
 predikce=initial_reference('databaze.hdf', vzorny_pozorovatel)
+
+###
+# 2. a flat initial series
+
 # from sunspots import initial_flat
 # predikce=initial_flat('databaze.hdf', vzorny_pozorovatel)
+
+# variable "kralik" contains the name of the initial observer. "kralik" is the equivalent of "guinea pig" in terms of a "laboratory animal"
 kralik=vzorny_pozorovatel
 
-#####################
-# inicialni datova series. Bud jako prosty prumer vsech nebo jako jeden z vybranych pozorovatelu
-
-
+###
+# 3. a plain average (arithmentic mean across the observers)
 # from sunspots import initial_mean
 # predikce=initial_mean('databaze.hdf')
 # kralik='PRUMER'
 
 
+# save the initial series for the comparisons at the very end
 initial=copy.deepcopy(predikce)  # POZOR, s timto polem se dale nikde nepocita, az na konci, pro ucely porovnani puvodni a vysledne serie
 
+# starting date 
 startdat=min(predikce['Datum'])
 
-# Uriznout inicialni sadu jen na prvni interval, pak uz se bude doplnovat predpovedi
+# Cut the initial series only to the first W-long interval
 dat1=startdat
 dat2=startdat+W
 predikce=predikce[(predikce['Datum'] >= dat1) & (predikce['Datum'] < dat2)]
 
 #######################################
 
-# Hlavni cyklus cesta tam
+# MAIN CYCLE 
 
 #######################################
 
 
 ##########
-# pokus o vypocet nejistot 
-# nejistota funkce f(x_1, x_2, ...) s velicinami x_1, ... a jejich nejistotami sigma_x1, ... se vypocte jako
+# Uncertainties 
+# Uncertainty of function f(x_1, x_2, ...) with independent variables x_1, ... and their uncertainties sigma_x1, ... is given as
 # sigma_f = sqrt{ suma_i [ (\partial f / \partial x_i)^2 sigma_xi^2 ] }
-# takze pro R = 10*g + f
+# hence for R = 10*g + f
 # sigma_R = sqrt{ 10^2*sigma_g^2 + sigma_f^2 }
 #
-# nejprve zkopirovat puvodni pole kvuli konzistenci
+# first copy the existing structure for consistency
 nejistota=copy.deepcopy(predikce)
-# pak vynulovat
+# then zero it
 nejistota['g']=0
 nejistota['f']=0
 nejistota['R']=0
-# a vymazat sloupec Q, ktery je k nicemu
+# remove Q column, which is useless
 nejistota=nejistota.drop(columns=['Q'])
 
 # import code
 # code.interact(local=locals())
 
+# cycle variable "pruchod" contains the number of full iteration
 for pruchod in range(1,Npruchodu+1):
     #referencni=copy.deepcopy(testovani)
     # if pruchod == 1:
         # referencni.remove(kralik)
     # referencni.remove(kralik)
     print('*********************')
-    print('Pruchod c. '+str(pruchod))
-    print('Tam...')
+    print('Pass No. '+str(pruchod))
+    print('Following the arrow of time...')
     while (startdat+W+dW) < max(db['Datum']):
         dat1=startdat
         print(dat1, max(db['Datum']))
         dat2=startdat+W
-        # uriznout databazi jen na zvoleny casovy usek
+        # cut the database only to W-long window
         db1=db[(db['Datum'] >= dat1) & (db['Datum'] < dat2)]
 
 
-        # prepocitat koeficienty pro predikci
+        # compute coefficients for the prediction
         tablekf=pd.DataFrame(columns=[kralik], index=referencni)
         tablekg=pd.DataFrame(columns=[kralik], index=referencni)
         tablenf=pd.DataFrame(columns=[kralik], index=referencni)
         tableng=pd.DataFrame(columns=[kralik], index=referencni)
         jmeno1=kralik
-        # set1=db1[(db1['Pozorovatel'] == jmeno1)]
+        # subset of the initial series
         set1=predikce[(predikce['Datum'] >= dat1) & (predikce['Datum'] < dat2)]
         for jmeno2 in referencni:
+            # subset of the other used observer
             set2=db1[(db1['Pozorovatel'] == jmeno2)]   
-            # slouceni obou datasetu s prunikem po polich "on"  
+            # intersection of both on date  
             temp = pd.merge(set1, set2, how='inner', on=['Datum'])  # slucujeme po poli datum
             Q1=np.asarray(temp['Q_x'])
             Q2=np.asarray(temp['Q_y'])
             f1=np.asarray(temp['f_x'])
             f2=np.asarray(temp['f_y'])
+            # reduction to optimal observing conditions
             f1red=f1*(1-redukce1*redukce[1]*(5-Q1))
             f2red=f2*(1-redukce1*redukce[1]*(5-Q2))
+            # limiting to only observations above the thresholds
             mask = (f1 > minspots) & (f2 > minspots)
             junk=f1red[mask]/f2red[mask]    
             k=np.mean(junk[np.isfinite(junk)])
@@ -216,22 +227,18 @@ for pruchod in range(1,Npruchodu+1):
             table_k=pd.concat([table_k, pd.DataFrame({'Datum':[startdat+W/2], 'Jmeno':[jmeno2], 'k_f':[kf], 'k_g':[k], 'Pruchod':[pruchod-0.5]})])
         # code.interact(local=locals())
 
-        # posouvame se za hranici intervalu
+        # shift beyond the evaluation interval
         dat1=startdat+W
         dat2=startdat+W+dW
         # uriznout databazi jen na zvoleny casovy usek
         db2=db[(db['Datum'] >= dat1) & (db['Datum'] < dat2)]
-        # najit pozorovatele, kteri se daji pouzit
+        # find usable observers in the prediction window
         pozorovatele=db2['Pozorovatel']
         temp=list(pozorovatele.drop_duplicates())
         dostupni=copy.deepcopy(temp)
-        dostupni=[jmeno for jmeno in dostupni if jmeno in referencni] # pouzit jen takove pozorovatele, kteri jsou soucasne i mezi duveryhodnymi
+        dostupni=[jmeno for jmeno in dostupni if jmeno in referencni] # check if their observations are available
         #import code
         #code.interact(local=locals())
-        # if (kralik in dostupni):
-       #      dostupni.remove(kralik)
-        #if (kralik in dostupni) & (pruchod == 1):
-        #    dostupni.remove(kralik)
         print(dostupni)    
         if len(dostupni) == 0:
             break
@@ -249,13 +256,11 @@ for pruchod in range(1,Npruchodu+1):
             for iref in range(0,len(dostupni)):
                 ref=dostupni[iref]
                 if len(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['g']):
-                    # pokud existuje porovnavaci udaj, tak nastav patricne hodnoty g, f a vah. Prepocitej g a f pres tabulku
+                    # in the case there exists an observation, get g and f values and recompute them to the target using the g and f table
                     g[iref]=np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['g'])*tablekg[kralik][ref]
                     f[iref]=np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['f'])*tablekf[kralik][ref]
                     g_red[iref]=g[iref]*(1-redukce2*redukce[2]*(5-np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['Q'])))
                     f_red[iref]=f[iref]*(1-redukce2*redukce[1]*(5-np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['Q'])))
-                    # print((1-redukce[2]*(5-np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['Q']))))
-                    # print((1-redukce[1]*(5-np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['Q']))))
                     wg[iref]=tableng[kralik][ref]
                     wf[iref]=tablenf[kralik][ref]
             if pruchod > 1:
@@ -268,11 +273,6 @@ for pruchod in range(1,Npruchodu+1):
                 fsave=(nansum(f*(wf/nansum(wf))))
                 sigma_g=nanstd(g)
                 sigma_f=nanstd(f)
-            # if np.isfinite(gsave):
-                # import code
-                # code.interact(local=locals())
-            # gsave=(nansum(g*(wg/nansum(wg))))
-            # fsave=(nansum(f*(wf/nansum(wf))))
             if np.isfinite(gsave):
                 gsave=round(gsave)
             if np.isfinite(fsave):
@@ -291,10 +291,10 @@ for pruchod in range(1,Npruchodu+1):
 
     #######################################
 
-    # Hlavni cyklus cesta zpet
+    # Main cycle, going backwards
 
     #######################################
-    print('Zpet...')
+    print('Against the arrow of time...')
     startdat=max(predikce['Datum'])
     predikce_zpet=predikce[(predikce['Datum'] <= startdat) & (predikce['Datum'] > (startdat-W))]
     nejistota_zpet=nejistota[(nejistota['Datum'] <= startdat) & (nejistota['Datum'] > (startdat-W))]
@@ -306,17 +306,15 @@ for pruchod in range(1,Npruchodu+1):
         db1=db[(db['Datum'] > dat1) & (db['Datum'] <= dat2)]
 
 
-        # prepocitat koeficienty pro predikci
+        # recompute the conversion personal coefficients, the same procedure as before
         tablekf=pd.DataFrame(columns=[kralik], index=referencni)
         tablekg=pd.DataFrame(columns=[kralik], index=referencni)
         tablenf=pd.DataFrame(columns=[kralik], index=referencni)
         tableng=pd.DataFrame(columns=[kralik], index=referencni)
         jmeno1=kralik
-        # set1=db1[(db1['Pozorovatel'] == jmeno1)]
         set1=predikce_zpet[(predikce_zpet['Datum'] > dat1) & (predikce_zpet['Datum'] <= dat2)]
         for jmeno2 in referencni:
             set2=db1[(db1['Pozorovatel'] == jmeno2)]   
-            # slouceni obou datasetu s prunikem po polich "on"  
             temp = pd.merge(set1, set2, how='inner', on=['Datum'])  # slucujeme po poli datum
             Q1=np.asarray(temp['Q_x'])
             Q2=np.asarray(temp['Q_y'])
@@ -342,7 +340,6 @@ for pruchod in range(1,Npruchodu+1):
             table_k=pd.concat([table_k, pd.DataFrame({'Datum':[startdat-W/2], 'Jmeno':[jmeno2], 'k_f':[kf], 'k_g':[k], 'Pruchod':[pruchod]})])
         # code.interact(local=locals())
 
-        # posouvame se za hranici intervalu
         dat1=startdat-W-dW
         dat2=startdat-W
         # uriznout databazi jen na zvoleny casovy usek
@@ -352,10 +349,6 @@ for pruchod in range(1,Npruchodu+1):
         temp=list(pozorovatele.drop_duplicates())
         dostupni=copy.deepcopy(temp)
         dostupni=[jmeno for jmeno in dostupni if jmeno in referencni]
-        # if (kralik in dostupni):
-            # dostupni.remove(kralik)
-        #if (kralik in dostupni) & (pruchod == 1):
-        #    dostupni.remove(kralik)
 
         # code.interact(local=locals())
         for irow in range(0,len(db2)):
@@ -374,8 +367,6 @@ for pruchod in range(1,Npruchodu+1):
                     f[iref]=np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['f'])*tablekf[kralik][ref]
                     g_red[iref]=g[iref]*(1-redukce2*redukce[2]*(5-np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['Q'])))
                     f_red[iref]=f[iref]*(1-redukce2*redukce[1]*(5-np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['Q'])))
-                    # print((1-redukce[2]*(5-np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['Q']))))
-                    # print((1-redukce[1]*(5-np.asarray(db2[(db2['Pozorovatel']==ref) & (db2['Datum']==date)]['Q']))))
                     wg[iref]=tableng[kralik][ref]
                     wf[iref]=tablenf[kralik][ref]
             if pruchod > 1:
@@ -388,8 +379,6 @@ for pruchod in range(1,Npruchodu+1):
                 fsave=(nansum(f*(wf/nansum(wf))))
                 sigma_g=nanstd(g)
                 sigma_f=nanstd(f)
-            # gsave=(nansum(g*(wg/nansum(wg))))
-            # fsave=(nansum(f*(wf/nansum(wf))))
             if np.isfinite(gsave):
                 gsave=round(gsave)
             if np.isfinite(fsave):
@@ -404,19 +393,30 @@ for pruchod in range(1,Npruchodu+1):
         startdat=startdat-dW
         
     # code.interact(local=locals())
-    # temp = pd.merge(predikce, predikce_zpet, how='inner', on=['Datum'])
-    # junk = np.asarray(temp['R_y']-temp['R_x'])
 
+    # the cycle is finished. Copy structures to go for the next cycle or for plotting/saving/etc.
     predikce=copy.deepcopy(predikce_zpet)
     nejistota=copy.deepcopy(nejistota_zpet)
     startdat=min(predikce['Datum'])
 
+# store to the files
+# the series
 predikce.to_hdf('X_predikce.hdf','predikce', mode='w')
+# its uncertainty
 nejistota.to_hdf('X_nejistota.hdf','nejistota', mode='w')
+# conversion coefficients
 table_k.to_hdf('X_predikce_tabulky_k.hdf','table_k', mode='w')
 
+####
+# 
+# THE CODE IS DONE HERE. WHAT FOLLOWS IS PURELY OPTIONAL
+#
+####
+#
+# PLOTTING, COMPARSION etc 
+#
 ###########################################################
-# porovnat se SIDC
+# compare to WDC-SILSO
 sunspot_number = pd.read_hdf('SN.hdf','sunspot_number')
   
 final_time=pd.date_range(start=min(predikce_zpet['Datum']), end=max(predikce_zpet['Datum']), freq='1D')
@@ -431,8 +431,7 @@ rozptyl_kg=np.empty(len(final_time))
 
 Whalf=dt.timedelta(days=13)
 
-#import code
-#code.interact(local=locals())
+#loop to fix the time axis, average over 13 days both
 for iday in range(0,len(final_time)):
     datum=final_time[iday]
     temp=predikce_zpet[(predikce_zpet['Datum'] >= (datum-Whalf)) & (predikce_zpet['Datum'] <= (datum+Whalf))]
@@ -455,14 +454,15 @@ for iday in range(0,len(final_time)):
         rozptyl_kf[iday]=np.std(kf)
         rozptyl_kg[iday]=np.std(kg)    
 
+# plot an output file, PDF with pages
 with PdfPages('X_predikce.pdf') as pdf:   # jedno PDF s vice stranami
     # code.interact(local=locals())
     plt.figure()
     plt.plot(final_time, reference_R, 'r')
     plt.plot(final_time, final_R)
-    plt.xlabel('Datum')
-    plt.ylabel('Relativni cislo')
-    plt.legend(['SIDC', 'Franta'])
+    plt.xlabel('Date')
+    plt.ylabel('Relative number')
+    plt.legend(['SIDC', 'our'])
     # plt.show()
     pdf.savefig()
     plt.close()
@@ -473,8 +473,8 @@ with PdfPages('X_predikce.pdf') as pdf:   # jedno PDF s vice stranami
     win=np.isfinite(final_R) & np.isfinite(initial_R)
     C=np.corrcoef(initial_R[win], final_R[win])
     plt.title('$\\rho$ = '+str(C[0,1]))
-    plt.xlabel('Datum')
-    plt.ylabel('Relativni cislo')
+    plt.xlabel('Date')
+    plt.ylabel('Relative number')
     plt.legend(['Initial', 'Final'])
     #plt.show()
     pdf.savefig()
@@ -483,9 +483,9 @@ with PdfPages('X_predikce.pdf') as pdf:   # jedno PDF s vice stranami
     win=np.isfinite(final_R) & np.isfinite(reference_R)
     C=np.corrcoef(reference_R[win], final_R[win])
     plt.plot(reference_R, final_R, '.')
-    plt.xlabel('Referencni R_i')
-    plt.ylabel('Frantovo R')
-    plt.title('Korelacni koeficient '+str(C[0,1]))
+    plt.xlabel('SILSO')
+    plt.ylabel('Our')
+    plt.title('Correlation coefficient '+str(C[0,1]))
     # plt.show()
     pdf.savefig()
     plt.close()
@@ -497,8 +497,8 @@ with PdfPages('X_predikce.pdf') as pdf:   # jedno PDF s vice stranami
         plt.plot(temp['Datum'], temp['k_f'], '--bo')
         plt.plot(temp['Datum'], temp['k_g'], '--ro')
         plt.legend(['$k_f$', '$k_g$'])
-        plt.xlabel('Datum')
-        plt.ylabel('Prevodni koeficienty pozorovatele')
+        plt.xlabel('Date')
+        plt.ylabel('Conversion coefficients of the observer')
         plt.title(jmeno)
         pdf.savefig()
         plt.close()
@@ -506,39 +506,13 @@ with PdfPages('X_predikce.pdf') as pdf:   # jedno PDF s vice stranami
     plt.figure()
     plt.errorbar(final_time, prumerne_kg, yerr=rozptyl_kg)
     plt.xlabel('Datum')
-    plt.ylabel('Rozptyl prevodnich koeficientu pro $g$')
+    plt.ylabel('Spread of the conversion coefficients for $g$')
     pdf.savefig()
     plt.close()
     #
     plt.figure()
     plt.errorbar(final_time, prumerne_kf, yerr=rozptyl_kf)
     plt.xlabel('Datum')
-    plt.ylabel('Rozptyl prevodnich koeficientu pro $f$')
+    plt.ylabel('Spread of the conversion coefficients for  $f$')
     pdf.savefig()
     plt.close()
-
-    
-# import code
-# code.interact(local=locals())
-
-  #
-# prumerne_kf=np.empty(len(final_time))*np.nan
-# prumerne_kg=np.empty(len(final_time))*np.nan
-# rozptyl_kf=np.empty(len(final_time))
-# rozptyl_kg=np.empty(len(final_time))
-# for iday in range(0,len(final_time)):
-#     temp=table_k[(table_k['Datum']==final_time[iday]) & (table_k['Pruchod']==Npruchodu)]
-#     if len(temp)>0:
-#         kf=np.asarray(temp['k_f'])
-#         kg=np.asarray(temp['k_g'])
-#         kf=kf[np.isfinite(kf)]
-#         kg=kg[np.isfinite(kg)]
-#         prumerne_kf[iday]=np.nanmean(kf)
-#         prumerne_kg[iday]=np.nanmean(kg)
-#         rozptyl_kf[iday]=np.std(kf)
-#         rozptyl_kg[iday]=np.std(kg)
-    
-# plt.errorbar(final_time, prumerne_kg, yerr=rozptyl_kg)
-# plt.errorbar(final_time, prumerne_kf, yerr=rozptyl_kf)
-    
- # plt.plot(final_time, prumerne_kg, '.')    
